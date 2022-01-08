05f31c302ec4e03e68ccfee24555a93cad1f6227
krzysztof.nadlonek <k.nadlonek@bsgroup.eu>
2022-01-08 10:33:47 +0100

    app setup

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SceneBasics from "./SceneBasics/SceneBasics";
+
+const App = () => {
+  return (
+    <div className="appContainer">
+      <SceneBasics />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/SceneBasics/SceneBasics.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SceneBasics = () => {
+  return (
+    <div>
+      <h1>Scene Basics</h1>
+    </div>
+  );
+};
+
+export default SceneBasics;
