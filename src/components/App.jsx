a502b1721330c3321009eca3040991cc609e8698
krzysztof.nadlonek <k.nadlonek@bsgroup.eu>
2022-01-09 00:09:20 +0100

    floor bulb castShadow receieveShadow

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import SceneBasics from "./SceneBasics/SceneBasics";
 import SceneBasicsFiber from "./SceneBasics/SceneBasicsFiber";
 import Geometry from "./GeometryLightMaterialsTextures/Geometry";
+import LightsShadows from "./GeometryLightMaterialsTextures/LightsShadows";
 
 const App = () => {
   return (
     <div className="appContainer">
       {/* <SceneBasics /> */}
       {/* <SceneBasicsFiber /> */}
-      <Geometry />
+      {/* <Geometry /> */}
+      <LightsShadows />
     </div>
   );
 };
